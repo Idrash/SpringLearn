4d6c8bc74fb1e979a91afe331fbdeec25684f966
Idrash <tognetti.yohann@outlook.fr>
2021-03-20 19:24:08 +0100

    Creation d'un path pour ajouter un role

--- SpringAuthentification/src/main/java/org/idrash/controllers/HomeController.java
+++ SpringAuthentification/src/main/java/org/idrash/controllers/HomeController.java
@@ -6,6 +6,8 @@ import org.idrash.persistence.models.User;
 import org.idrash.persistence.repository.AuthorityRepository;
 import org.idrash.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -51,6 +53,21 @@ public class HomeController {
     @ResponseBody
     public String getRessource(){
         return "hello world";
+    }
+
+    @RequestMapping(value = "/role")
+    @ResponseBody
+    public String setRole(){
+        Authority authority = authorityRepository.getOne(1);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user =userRepository.findByUsername(authentication.getName());
+        if(user==null){
+            return "user not found";
+        }
+        user.getAuthorities().add(authority);
+        userRepository.save(user);
+        return "role "+authority.getAuthority()+" to the user : "+user.getUsername();
     }
 
 
--- SpringAuthentification/src/main/java/org/idrash/persistence/models/User.java
+++ SpringAuthentification/src/main/java/org/idrash/persistence/models/User.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Table(name = "user")
 public class User{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false, unique = true)
